4be3da3774821f672555ad21359e4f41e4779b7b
zhhraid <zhahraastwoti@gmail.com>
2025-12-22 13:30:32 +0700

    Add missing DELETE route for event deletion

--- routes/events.js
+++ routes/events.js
@@ -16,4 +16,7 @@ router.get('/my-events/:userId', eventController.getMyCreatedEvents);
 // [PUT] /api/events/:id (Update Event)
 router.put('/:id', eventController.updateEvent);
 
+// [DELETE] /api/events/:id (Delete Event)
+router.delete('/:id', eventController.deleteEvent);
+
 module.exports = router;
